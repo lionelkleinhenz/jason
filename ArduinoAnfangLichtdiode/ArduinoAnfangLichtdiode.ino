3ebabff2d82ca1d4e5b73f0105ce25e3fee27ec1
PlaySwitchBox <152816356+PlaySwitchBox@users.noreply.github.com>
2026-04-29 15:00:53 +0200

    Update ArduinoAnfangLichtdiode.ino

--- ArduinoAnfangLichtdiode/ArduinoAnfangLichtdiode.ino
+++ ArduinoAnfangLichtdiode/ArduinoAnfangLichtdiode.ino
@@ -1,10 +1,9 @@
 void setup() {
   pinMode(9, OUTPUT);
 }
-
 void loop() {
   int lichtWert = analogRead(A0);
   int stromStaerke = map(lichtWert, 0, 1023, 255, 0);
-  analogWrite(9, stromStaerke);
   delay(100);
+  serial.println(stromStaerke)
 }
