7a86c2f8c3196bd0384b7e2539d48d5da35854fe
PlaySwitchBox <152816356+PlaySwitchBox@users.noreply.github.com>
2026-04-29 14:45:03 +0200

    Remove analogWrite for stromStaerke

--- ArduinoAnfangLichtdiode/ArduinoAnfangLichtdiode.ino
+++ ArduinoAnfangLichtdiode/ArduinoAnfangLichtdiode.ino
@@ -6,6 +6,5 @@ void loop() {
   int lichtWert = analogRead(A0);
   int stromStaerke = 255 - (int) ((255.0 / 1023.0) * lichtWert);
   stromStaerke = constrain(stromStaerke, 0, 255);
-  analogWrite(9, stromStaerke);
   delay(100);
 }
